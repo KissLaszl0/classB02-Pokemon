56c51cf999daf64816ea36032b2eb8502ef35e7c
Laszlo Kiss <boltfigyelo@gmail.com>
2021-10-24 21:22:44 +0200

    init AttackSkill class

---
+++ AttackSkill.js
@@ -0,0 +1,36 @@
+class AttackSkill
+{
+    constructor(name, damage, magic, eigenschaft)
+    {
+    this.name = name;
+    this.damage = damage;
+    this.magic = magic;
+    this.eigenschaft = eigenschaft;
+    }
+
+    getATTDamage()
+    {
+        return this.damage;
+    }
+    getATTMagic()
+    {
+        return this.magic;
+    }
+    getATTName()
+    {
+        return this.name;
+    }
+    showAttack()
+    {
+        return `
+        Skill name: ${this.name}
+        damage: ${this.damage}
+        magic: ${this.magic}
+        eigenschaft: ${this.eigenschaft}
+        `
+    }
+
+}
+
+
+module.exports = AttackSkill;
